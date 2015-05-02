ba34eaf49fd167f96b993f94a144febb5bcc6e6c
Mallory <mallory@Mallorys-iMac.home>
2015-05-02 11:45:25 -0700

    Initial commit

---
+++ keeper.js
@@ -0,0 +1,13 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
